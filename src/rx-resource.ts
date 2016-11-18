d0de86d9d7e2427f350f641c092d60334bf005f2
Manuel Pacheco <manuelalejandropm@gmail.com>
2016-11-18 17:29:52 -0400

    add support for typedoc documentation

--- src/rx-resource.ts
+++ src/rx-resource.ts
@@ -1,5 +1,8 @@
 import { RestConfig } from './interfaces';
 
+/**
+ * Central Repository for RxResources
+ */
 export class RxResource {
 
   constructor(public type: string, config: RestConfig) { }
