fcab1924203cda896b9e6cf7986a37ddb0ffb612
Manuel Pacheco <manuelalejandropm@gmail.com>
2016-11-21 11:39:09 -0400

    initial expansion of RestConfig interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,1 +1,13 @@
-export interface RestConfig { }
+import { Observable } from 'rxjs';
+
+export interface RestConfig {
+  requester?: RequestDelegate;
+}
+
+export interface RequestDelegate {
+  get(url: string, args?: any): Observable<any>;
+  post(url: string, body: any, args?: any): Observable<any>;
+  put?(url: string, body: any, args?: any): Observable<any>;
+  patch(url: string, body: any, args?: any): Observable<any>;
+  delete(url: string, args?: any): Observable<any>;
+}
