a1b36cc985bfadad9fd94c590adbbfab9e5097ba
Manuel Pacheco <manuelalejandropm@gmail.com>
2016-11-21 14:59:00 -0400

    add basic implementations of find and findAll

--- src/rx-resource.ts
+++ src/rx-resource.ts
@@ -1,9 +1,22 @@
-import { RestConfig } from './interfaces';
+import { RestConfig, RequestDelegate } from './interfaces';
+import { Observable } from 'rxjs';
 
 /**
  * Central Repository for RxResources
  */
-export class RxResource {
+export class RxResource<T> {
 
-  constructor(public type: string, config: RestConfig) { }
+  constructor(public type: string, private config: RestConfig) { }
+
+  find(id: any): Observable<T> {
+    let url = `${this.config.baseUrl}/${this.type}/${id.toString()}`;
+    // TODO FIX CASTING
+    return (this.config.requester as RequestDelegate).get(url);
+  }
+
+  findAll(): Observable<T[]> {
+    let url = `${this.config.baseUrl}/${this.type}`;
+    // TODO FIX CASTING
+    return (this.config.requester as RequestDelegate).get(url);
+  }
 }
